0002505837f42631cd060dcba464ea77ac0c7b5c
Ali Dorri <53428614+A-Dorri@users.noreply.github.com>
2023-01-21 16:10:24 -0800

    Reformat the code text

--- lua/driver.py
+++ lua/driver.py
@@ -13,20 +13,24 @@ L_INS = len(K.INSTRUCTIONS)
 
 lua_p = 'compiled.luap'
 
-def generate_random_instruction(): # 4-byte instruction
+
+def generate_random_instruction():  # 4-byte instruction
     for i in range(4): yield random.randint(0, 255)
+
 
 def create_body(instr_count):
     for k in range(instr_count): yield from generate_random_instruction()
 
+
 def create_lua_binary(instruction_seq):
     instr_count = int(len(instruction_seq) / 4)
     binary_form = (bytes(K.PREFIX) +
-                  (instr_count + 1 + 3).to_bytes(4, byteorder='little') +
-                  bytes(instruction_seq + K.PRINT + K.RETURN_INSTRUCTION + K.POSTFIX))
+                   (instr_count + 1 + 3).to_bytes(4, byteorder='little') +
+                   bytes(instruction_seq + K.PRINT + K.RETURN_INSTRUCTION + K.POSTFIX))
     with open(lua_p, 'bw') as binary_file:
         binary_file.write(binary_form)
     return instruction_seq
+
 
 def execute_binary():
     try:
@@ -41,6 +45,7 @@ def execute_binary():
     else:
         return stderr
 
+
 def validate_lua(input_str, log_level):
     """ return:
         rv: "complete", "incomplete" or "wrong",
@@ -53,7 +58,7 @@ def validate_lua(input_str, log_level):
         output = execute_binary()
         print(repr(output))
         if output == "complete":
-            return "complete",-1,""
+            return "complete", -1, ""
         elif output == "incomplete":
             return "incomplete", -1, ""
         else:
@@ -64,12 +69,14 @@ def validate_lua(input_str, log_level):
         n = len(msg)
         return "wrong", n, ""
 
+
 def get_next_char(log_level, pool):
     if L_INS - len(pool) > MAX_TRIES:
         return None
     print(len(pool), end=' ')
     input_char = pool.pop()
     return input_char
+
 
 def generate(log_level, misbehaving_ins):
     """
@@ -92,7 +99,7 @@ def generate(log_level, misbehaving_ins):
         rv, n, c = validate_lua(curr_str, log_level)
 
         if log_level:
-            print("%s n=%d, c=%s. Input string is %s" % (rv,n,c,curr_str))
+            print("%s n=%d, c=%s. Input string is %s" % (rv, n, c, curr_str))
         if rv == "complete":
             if random.randrange(5) == 0:
                 return curr_str
@@ -101,7 +108,7 @@ def generate(log_level, misbehaving_ins):
                 random.shuffle(pool)
                 prev_str = curr_str
                 continue
-        elif rv == "incomplete": # go ahead...
+        elif rv == "incomplete":  # go ahead...
             print('.', end='')
             if len(curr_str) >= MAX_LEN:
                 return curr_str
@@ -109,19 +116,20 @@ def generate(log_level, misbehaving_ins):
             random.shuffle(pool)
             prev_str = curr_str
             continue
-        elif rv == "wrong": # try again with a new random character do not save current character
+        elif rv == "wrong":  # try again with a new random character do not save current character
             continue
         else:
             print("ERROR What is this I dont know !!!")
             break
     return None
 
+
 def create_valid_strings(n, log_level):
     os.remove("valid_inputs.txt") if os.path.exists('valid_inputs.txt') else None
     os.remove("misbehaving_instructions.txt") if os.path.exists('misbehaving_instructions.txt') else None
     tic = time.time()
     i = 0
-    while True: # while
+    while True:  # while
         i += 1
         misbehaving_ins = []
         created_string = generate(log_level, misbehaving_ins)
@@ -131,5 +139,6 @@ def create_valid_strings(n, log_level):
                 var = f"Time used until input was generated: {toc - tic:f}\n" + repr(created_string) + "\n\n"
                 myfile.write(var)
                 [file.write(str(char) + "\n") for char in misbehaving_ins]
+
 
 create_valid_strings(10, 0)
--- python/driver.py
+++ python/driver.py
@@ -44,18 +44,20 @@ k = bytes([%s])
 v = f.__code__.replace(co_code=bytes(k))
 exec(v)
 """
-#import marshal
-#marshal.dump(f.__code__, open('f.dump', 'wb+'))
-#code = marshal.load(open('test.dump'))
-#f.__code__ == code
+# import marshal
+# marshal.dump(f.__code__, open('f.dump', 'wb+'))
+# code = marshal.load(open('test.dump'))
+# f.__code__ == code
 
 python_p = 'compiled.p'
 
+
 def create_python_binary(instruction_seq):
-    instructions = [str(i) for i in  (K.PREFIX +
-                  (instruction_seq + K.PRINT + K.SUFFIX))]
+    instructions = [str(i) for i in (K.PREFIX +
+                                     (instruction_seq + K.PRINT + K.SUFFIX))]
     with open(python_p, 'w+') as file:
         file.write(template % ','.join(instructions))
+
 
 def execute_binary():
     try:
@@ -67,18 +69,19 @@ def execute_binary():
     if stdout[-3:] == 'end':
         return 'incomplete'
     elif stdout == '':
-        if result.returncode == -11: # segfault
+        if result.returncode == -11:  # segfault
             return 'error'
-        elif result.returncode == -10: # segfault
+        elif result.returncode == -10:  # segfault
             return 'error'
-        elif result.returncode < 0: # segfault
+        elif result.returncode < 0:  # segfault
             return 'error'
         else:
-           # if it returns without end but without signal, then it is complete
-           print(result.returncode, "complete")
-           return 'complete'
+            # if it returns without end but without signal, then it is complete
+            print(result.returncode, "complete")
+            return 'complete'
     else:
         return stderr
+
 
 def validate_python(input_str, log_level):
     """ return:
@@ -103,12 +106,14 @@ def validate_python(input_str, log_level):
         n = len(msg)
         return "wrong", n, ""
 
+
 def get_next_char(log_level, pool):
     if L_INS - len(pool) > MAX_TRIES:
         return None
     print(len(pool), end=' ')
     input_char = pool.pop()
     return input_char
+
 
 def generate(log_level, misbehaving_ins):
     """
@@ -131,7 +136,7 @@ def generate(log_level, misbehaving_ins):
         rv, n, c = validate_python(curr_str, log_level)
 
         if log_level:
-            print("%s n=%d, c=%s. Input string is %s" % (rv,n,c,curr_str))
+            print("%s n=%d, c=%s. Input string is %s" % (rv, n, c, curr_str))
         if rv == "complete":
             if random.randrange(5) == 0:
                 return curr_str
@@ -140,7 +145,7 @@ def generate(log_level, misbehaving_ins):
                 random.shuffle(pool)
                 prev_str = curr_str
                 continue
-        elif rv == "incomplete": # go ahead...
+        elif rv == "incomplete":  # go ahead...
             print('.', end='')
             if len(curr_str) >= MAX_LEN:
                 return curr_str
@@ -148,19 +153,20 @@ def generate(log_level, misbehaving_ins):
             random.shuffle(pool)
             prev_str = curr_str
             continue
-        elif rv == "wrong": # try again with a new random character do not save current character
+        elif rv == "wrong":  # try again with a new random character do not save current character
             continue
         else:
             print("ERROR What is this I dont know !!!")
             break
     return None
 
+
 def create_valid_strings(n, log_level):
     os.remove("valid_inputs.txt") if os.path.exists('valid_inputs.txt') else None
     os.remove("misbehaving_instructions.txt") if os.path.exists('misbehaving_instructions.txt') else None
     tic = time.time()
     i = 0
-    while True: # i < 10
+    while True:  # i < 10
         i += 1
         misbehaving_ins = []
         created_string = generate(log_level, misbehaving_ins)
@@ -171,5 +177,5 @@ def create_valid_strings(n, log_level):
                 myfile.write(var)
                 [file.write(str(char) + "\n") for char in misbehaving_ins]
 
-create_valid_strings(10, 0)
 
+create_valid_strings(10, 0)
